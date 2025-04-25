44384e52ed0987471512e6b759ec17fefdd7b742
DonAlberton <szpejewski.alberto@gmail.com>
2025-04-25 12:41:47 +0200

    Add automation to building and pushing docker images to GCP

---
+++ docker-build-push-gcp.sh
@@ -0,0 +1,10 @@
+PREFIX="europe-central2-docker.pkg.dev/playground-454021/playit"
+docker build -t ${PREFIX}/classifier ./classifier
+docker build -t ${PREFIX}/scheduler ./scheduler
+docker build -t ${PREFIX}/playit_api ./playit_api
+docker build -t ${PREFIX}/playit_api_gateway ./playit_api_gateway
+
+docker push ${PREFIX}/classifier
+docker push ${PREFIX}/scheduler
+docker push ${PREFIX}/playit_api
+docker push ${PREFIX}/playit_api_gateway
